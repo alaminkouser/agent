36155f6c70cc6727cd23e693ece3c9f1ae94584f
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-04-18 21:36:18 +0600

    feat: integrate Gemini API with Google Search and MCP tools for message processing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import datetime
-from utilities.mcp_connection_manager import MCPConnectionManager
 import asyncio
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from utilities.telegram_bot import handle_message
@@ -12,11 +11,6 @@ load_dotenv()
 TELEGRAM_BOT = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
 
 async def main():
-    MCP = MCPConnectionManager()
-    MCP_SESSION = await MCP.connect()
-
-    TELEGRAM_BOT.bot_data["mcp_session"] = MCP_SESSION
-
     TELEGRAM_BOT.add_handler(
         MessageHandler(
             filters.ALL,
@@ -24,8 +18,6 @@ async def main():
         )
     )
 
-    
-    await MCP.disconnect()
 
 if __name__ == "__main__":
     asyncio.run(main())
