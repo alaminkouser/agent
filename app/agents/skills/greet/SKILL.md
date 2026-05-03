c75927b84cd35f5fa9a0b7852844560421c3ee3a
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-05-04 01:10:08 +0600

    added a skill with script and fixed instructions

---
+++ app/agents/skills/greet/SKILL.md
@@ -0,0 +1,11 @@
+---
+name: greet
+description: I greet people.
+---
+
+# How to Greet Someone
+
+To greet some one use the `greet` script. These arguments are required:
+
+- name-first
+- name-last
---
+++ app/agents/skills/greet/scripts/greet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+from dataclasses import dataclass
+
+@dataclass
+class GreetInput:
+    name_first: str
+    name_last: str
+
+def greet(input: GreetInput) -> str:
+    return f"Hello {input.name_first}. Your full name is: {input.name_first} {input.name_last}."
+
+def main() -> str:
+    parser = argparse.ArgumentParser(description="Greet People")
+    parser.add_argument(
+        "--name-first",
+        type=str,
+        required=True,
+        help="First Name",
+    )
+    parser.add_argument(
+        "--name-last",
+        type=str,
+        required=True,
+        help="Last Name",
+    )
+
+    args = parser.parse_args()
+    print(greet(input=GreetInput(
+        name_first=args.name_first,
+        name_last=args.name_last
+    )))
+
+if __name__ == "__main__":
+    main()
